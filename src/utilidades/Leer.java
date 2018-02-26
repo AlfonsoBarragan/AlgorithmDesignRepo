18f177d86ccc2e64af5ea6fa2bc3b9c2385178f4
RoberPlaza <Rober.Plaza@alu.uclm.es>
2018-02-26 11:33:55 +0100

    He arreglado la clase putamente horrenda

--- src/utilidades/Leer.java
+++ src/utilidades/Leer.java
@@ -27,15 +27,18 @@ public class Leer {
 
 // nextDouble() nextLine() leer.next();
 
-private static final Scanner teclado=new Scanner(System.in);
+private static final Scanner teclado = new Scanner(System.in);
 
 public static String cadena(){
+    
       return teclado.next();
   }
   public static char caracter(){
+      
       return caracter("");
   }
   public static char caracter(String s){
+      
       System.out.println(s);
       return teclado.next().charAt(0);
   }
@@ -43,69 +46,100 @@ public static String cadena(){
 
 
   public static int entero() throws Exception{
+      
     return entero("");
+    
   }
   public static int entero(String s) throws Exception{
-    int res=0;
-    boolean vale=true;
-    do{
-      vale=true;
+      
+    int res = 0;
+    boolean vale = true;
+    
+    do {
+        
+      vale = true;
       System.out.println(s);
-      try{
-        res=teclado.nextInt();
+      try {
+          
+        res = teclado.nextInt();
+        
       }
       catch (Exception e) {
-        vale=false;
+          
+        vale = false;
         teclado.next();
       }
-    }while(!vale);
+      
+    } while(!vale);
+    
     teclado.nextLine();
     return res;
   }
-  public static int entero(String s, int min, int max)throws Exception{
+  public static int entero(String s, int min, int max) throws Exception {
+      
     int res=min-1;
+    
     while(res<min || res>max)
-    res=entero(s+"["+min+","+max+"]");
+        res=entero(s+"["+min+","+max+"]");
+    
     return res;
+    
   }
 public static double continuo() throws Exception{
+    
     return continuo("");
   }
   public static double continuo(String s) throws Exception{
-    double res=0;
-    boolean vale=true;
-    do{
-      vale=true;
+      
+    double res = 0;
+    boolean vale = true;
+    
+    do {
+        
+      vale = true;
       System.out.println(s);
-      try{
-        res=teclado.nextDouble();
+      
+      try {
+        res = teclado.nextDouble();
       }
       catch (Exception e) {
-        vale=false;
+        
+        vale = false;
         teclado.next();
       }
-    }while(!vale);
-    teclado.nextLine();
+    }
+    while(!vale);
+        teclado.nextLine();
+        
     return res;
   }
+  
    public static long grande() throws Exception{
     return grande("");
   }
   public static long grande(String s) throws Exception{
-    long res=0;
-    boolean vale=true;
-    do{
-      vale=true;
+      
+    long res = 0;
+    boolean vale = true;
+    
+    do {
+        
+      vale = true;
       System.out.println(s);
-      try{
-        res=teclado.nextLong();
+      
+      try {
+        res = teclado.nextLong();
       }
+      
       catch (Exception e) {
-        vale=false;
+        vale = false;
         teclado.next();
       }
-    }while(!vale);
+    }
+    
+    while(!vale);
     teclado.nextLine();
+        
     return res;
   }
 
