e661b8b4a7b022635ec33c569f295261cb61ef03
RoberPlaza <Rober.Plaza@alu.uclm.es>
2018-03-12 13:30:21 +0100

    Practica ya funcionando

--- src/practica1_numerocombinatorio/Algoritmos.java
+++ src/practica1_numerocombinatorio/Algoritmos.java
@@ -1,47 +1,94 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practica1_numerocombinatorio;
-
+ 
+import java.util.Stack;
+ 
 /**
  *
  * @author alf
  */
 public class Algoritmos {
-    
-    
+   
+    public static long combinatorioPilas(int n, int k) {
+       
+        int sol = 40;
+        
+        Stack<Integer> sN =         new Stack<Integer>();
+        Stack<Integer> sK =         new Stack<Integer>();
+        Stack<Integer> sResults =   new Stack<Integer>();
+        Stack<Integer> sCalls =     new Stack<Integer>();
+       
+        sN.push(n);
+        sK.push(k);
+        sCalls.push(1);
+        sResults.push(0);
+       
+        while (!sN.isEmpty()) {
+            while ( (sK.peek() > 0) && (sN.peek() != sK.peek()) && 
+                    sCalls.peek() <= 2) {
+               
+                switch(sCalls.peek()) {
+                    case 1:
+                        sN.push(sN.peek() - 1);
+                        sK.push(sK.peek() - 1);
+                        break;
+                    
+                    case 2:
+                        sN.push(sN.peek() -1);
+                        sK.push(sK.peek());
+                        break;
+                }
+                sCalls.push(1);
+                if(sK.peek() == 0 || sN.peek() == sK.peek()){
+                    sResults.push(1);
+                } else {
+                    sResults.push(0);
+                }
+            }
+               
+            sN.pop();
+            sK.pop();
+            sCalls.pop();
+            sol = sResults.pop();
+               
+            if(!sN.empty()) {
+                sCalls.push(sCalls.pop() + 1);
+                sResults.push(sResults.pop() + sol);
+            }
+        }
+        if(sol == 0) sol = 1;
+        return sol;
+    }
     public static int combinatorioRecursivo(int n, int k){
+        
         if(k==0 || k==n){
             return 1;
         }else{
             return combinatorioRecursivo(n-1,k-1)+ combinatorioRecursivo(n-1,k);
         }
     }
-    
+   
     public static long combinatorioIterativo(int n, int k){
         long factorialk = 1;
         long factorialn = 1;
         long factorialnk = 1;
-
+ 
         //Esto saca el factorial de n
         for (int i = 1; i < n+1; i++) {
             factorialn = i * factorialn;
         }
-
+ 
         //Esto saca el factorial de k
         for (int i = 1; i < k+1; i++) {
             factorialk = i * factorialk;
         }
-
+ 
         //Esto saca el factorial de n-k:
         for (int i = 1; i < (n - k + 1); i++) {
             factorialnk = i * factorialnk;
         }
-
+       
+ 
         //Esto calcula el combinatorio de n & k:
         return(factorialn / (factorialk * factorialnk));
-
     }
 }
--- src/practica1_numerocombinatorio/Practica1_NumeroCombinatorio.java
+++ src/practica1_numerocombinatorio/Practica1_NumeroCombinatorio.java
@@ -4,12 +4,10 @@
  * and open the template in the editor.
  */
 package practica1_numerocombinatorio;
-
-import java.util.Scanner;
-
+import java.io.*;
 /**
  *
- * @author alf
+ * @author alf, rober
  */
 public class Practica1_NumeroCombinatorio {
 
@@ -18,22 +16,99 @@ public class Practica1_NumeroCombinatorio {
      */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
-        int n = 0;
-        int k = 0;
+        PrintWriter fichero = new PrintWriter("Benchmarks.txt", "UTF-8");
+        int  index = 0;
+        int[] n = new int[10], k = new int[10];
+        long rit, rrec, rpilas, itTime, reTime, stTime;
+        
+        System.out.println("1 para introducir manualmente 1 número");
+        System.out.println("2 para introducir manualmente n números");
+        System.out.println("3 para una prueba con valores por defecto");
+        
+        switch(utilidades.Leer.cadena()) {
+            
+            case "1":
+                index = 1;
+                manualSingle(n, k);
+                break;
+                
+            case "2":
+                System.out.println("Cuántos números quieres meter? (max 10)");
+                index = utilidades.Leer.entero();
+                manualSeveral(n, k, index);
+                break;
+                
+            case "3":
+                index = 4;
+                computerSample(n, k);
+                break;
+                
+            default:
+                System.out.println("Gracioso, que eres un gracioso");
+                System.exit(1);
+                break;        
+        }
+        
+        
+        for(int i = 0; i < index; i++){
+            
+            System.out.println("Calculando factorial de " + n[i] + ", " + k[i]);
+            fichero.printf("Calculando factorial de %d, %d\n", n[i], k[i]);
+            
+            System.out.print("Iterativo:");
+            itTime = System.currentTimeMillis();
+            rit = (Algoritmos.combinatorioIterativo(n[i], k[i]));
+            itTime = System.currentTimeMillis() - itTime;
+            fichero.println("Iterativo: Tiempo: " + itTime + "ms Resultado: " + rit);
+            System.out.println("Tiempo: " + itTime + "ms Resultado: " + rit);
+            
+            System.out.print("Recursivo:");
+            reTime = System.currentTimeMillis();
+            rrec = (Algoritmos.combinatorioRecursivo(n[i], k[i]));
+            reTime = System.currentTimeMillis() - reTime;
+            fichero.println("Recursivo: Tiempo: " + reTime + "ms Resultado: " + rrec);
+            System.out.println("Tiempo: " + reTime + "ms Resultado: " + rrec);
+            
+            System.out.print("Pilas:");
+            stTime = System.currentTimeMillis();
+            rpilas = Algoritmos.combinatorioPilas(n[i], k[i]);
+            stTime = System.currentTimeMillis() - stTime;
+            fichero.println("Pilas: Tiempo: " + stTime + "ms Resultado: " + rpilas);
+            System.out.println("Tiempo: " + stTime + "ms Resultado: " + rpilas);
+            
+            System.out.println("\n");
+        }
+    }
+    
+    public static void manualSingle(int[] n, int[] k) throws Exception {
         
         System.out.println("Introduzca los valores para calcular numero combinatorio nCk");
+        
         System.out.println("*** Introduzca el valor de n ***");
         System.out.print("---->");
-        n = utilidades.Leer.entero();
+        n[0] = utilidades.Leer.entero();
         
         System.out.println("*** Introduzca el valor de k ***");
         System.out.print("---->");
-        k = utilidades.Leer.entero();
-       
-        System.out.println(Algoritmos.combinatorioIterativo(n, k));
-        
-        System.out.println(Algoritmos.combinatorioRecursivo(n, k));
-    
+        k[0] = utilidades.Leer.entero();
     }
     
+    public static void manualSeveral(int[] n, int[] k, int index) throws Exception {
+        
+        for(int i = 0; i < index; i++){
+            System.out.println("*** Introduzca el valor de n ***");
+            System.out.print("---->");
+            n[i] = utilidades.Leer.entero();
+            
+            System.out.println("*** Introduzca el valor de k ***");
+            System.out.print("---->");
+            k[i] = utilidades.Leer.entero();
+        }
+    }
+    
+    public static void computerSample(int[] n, int[] k) throws Exception {
+        
+        n[0] = 40; n[1] = 10; n[2] = 20; n[3] = 50;
+        k[0] = 1; k[1] = 3; k[2] = 10; k[3] = 5;
+    }
 }
